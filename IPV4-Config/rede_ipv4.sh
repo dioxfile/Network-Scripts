990d45c85deb492dbb17914019c52c37f5fde975
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2019-05-30 16:33:34 -0400

    Add files via upload

---
+++ IPV4-Config/rede_ipv4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export NIC=$(route -n | egrep "0.0.0.0" | awk '{print $8}' | sort -u) # With or not Gateway
+rede=$( dialog --stdout --inputbox 'Insert the Network Scope (ex. 10.0.0):' 0 0 )
+mask=$( dialog --stdout --inputbox 'Insert the Netmask /n format:' 0 0 )
+Quantity=$( dialog --stdout --inputbox 'Insert IP Quantity (n):'    0 0 )
+ifconfig $NIC down
+ifconfig $NIC up
+
+for conta in $(seq 1 $Quantity);
+do
+ifconfig $NIC:$conta $rede.$conta/$mask up
+done;
+
+ifconfig > NIC 
+dialog \
+--title 'Ifconfig Show'  \
+--textbox NIC \
+0 0
+rm NIC
