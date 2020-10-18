9695ba0ac0bb08d8ebde41fa8812a1df81c5d62d
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2020-10-18 10:08:25 -0400

    Add files via upload

---
+++ IPV4_Config/rede_ipv4_windows.bat
@@ -0,0 +1,26 @@
+@echo off
+choice /C FAC /M "Pressione: [F]ixo, [A]utomatico ou [C]ancelar"
+IF errorlevel=3 goto FIM
+IF errorlevel=2 goto AUTOMATICO
+IF errorlevel=1 goto FIXO
+
+:FIXO
+set /p INTERFACE=Nome da Interface--^>
+set /p IP=Ip--^>
+set /p MASCARA=Masc--^> 
+set /p GATEWAY=Gw--^>
+set /p DNS=Dns--^>
+netsh int ip set address name="%INTERFACE%" source=static %IP% %MASCARA% %GATEWAY% 1
+netsh int ip set dns name="%INTERFACE%" static %DNS% 
+goto :FIM
+
+:AUTOMATICO 
+netsh int ip delete dns name="Rede" all
+netsh int ip set address name="Rede" source=dhcp
+netsh int ip set dns name="Rede" dhcp
+goto :FIM
+
+:FIM
+@echo Finished
+ipconfig
+
