44468b17751ed3ca3bb11b2f998aaaee882538c9
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2020-10-19 11:48:01 -0400

    Update rede_ipv6_windows.bat

--- IPV6_Config/rede_ipv6_windows.bat
+++ IPV6_Config/rede_ipv6_windows.bat
@@ -15,19 +15,16 @@ set /p DNS2=Dns2--^>
  netsh interface ipv6 add dnsserver "%INTERFACE%" %DNS2% index=2
  netsh interface ipv6 add route %IP% interface="%INTERFACE%" %GATEWAY% publish=yes
 goto :FIM
-
  
-:AUTOMATICO 
+:AUTOMATICO
+FOR /F "tokens=3,*" %%A IN ('netsh interface ipv6 show interface^|find "Connected"') DO (set INTERFACE=%%B)
+netsh interface ipv6 delete address interface="%INTERFACE%"
+ipconfig /release6 
 ipconfig /renew6
-ipconfig /release6
 netsh interface ipv6 delete dnsserver "%INTERFACE%" all
-netsh interface ipv6 set int interface="%INTERFACE%" advertise=enable managed=enabled
+netsh interface ipv6 set int interface="%INTERFACE%" advertise=enable managed=enable
 goto :FIM
 
 :FIM
 @echo Finished
 ipconfig
-netsh interface ipv6 show dns "%INTERFACE%"
-netsh interface ipv6 show address "%INTERFACE%"
-
-
