305fb49d8347f4b66be4f6f09dda7723afbe0687
Diógenes Antonio Marques José <32777186+dioxfile@users.noreply.github.com>
2020-11-05 20:12:37 -0400

    Add files via upload

--- Iptables_Share_Internet/Iptables_Internet_Share.sh
+++ Iptables_Share_Internet/Iptables_Internet_Share.sh
@@ -13,12 +13,7 @@ ON_OFF_FORWARDING=$(dialog --stdout --inputbox 'Type 1 for enable Forwarding or 
 --title 'WARNING' \
 --msgbox 'ERROR: value can not by empty!!!' \
 0 0;exit; }
-if [ "$ON_OFF_FORWARDING" != "1" || "$ON_OFF_FORWARDING" != "0" ]; then
-{ dialog \
---title 'WARNING' \
---msgbox 'ERROR: value must be '1'or '0'!!! ' \
-0 0;exit; }
-fi
+
 if [ "$ON_OFF_FORWARDING" = "0" ]; then
 { dialog \
 --title 'AVISO' \
@@ -26,6 +21,7 @@ if [ "$ON_OFF_FORWARDING" = "0" ]; then
 0 0;exit; }
 fi
 
+if [ "$ON_OFF_FORWARDING" = "1" ]; then
 IFACE_IN=$( dialog --stdout --inputbox 'Type your LAN Iface:'    0 0 )
 [ "$IFACE_IN" ] ||
 { dialog \
@@ -39,7 +35,27 @@ IFACE_OUT=$( dialog --stdout --inputbox 'Type your WAN Iface'    0 0 )
 --msgbox 'ERROR: value can not by empty!!!' \
 0 0;exit; }
 
+modprobe iptable_nat    # Forwarding
+###Empty rules
+iptables -F         &&
+iptables -F INPUT   &&
+iptables -F OUTPUT  &&
+iptables -F FORWARD &&
+echo "Cleanning Rules "
+             
+#Changing Policies - first block everything and 
+#then release only what is needed.
+iptables -P INPUT DROP      &&
+iptables -P FORWARD ACCEPT  &&
+iptables -P OUTPUT ACCEPT   &&
+echo "Policies Default"
+
+#Loopback local process
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -i $IFACE_IN -j ACCEPT
+
 echo $ON_OFF_FORWARDING > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o $IFACE_OUT -j MASQUERADE
 iptables -A FORWARD -i $IFACE_IN -o $IFACE_OUT -j ACCEPT
 iptables -A FORWARD -i $IFACE_OUT -o $IFACE_IN -m state --state ESTABLISHED,RELATED \
          -j ACCEPT
@@ -51,8 +67,15 @@ iptables -A FORWARD -i $IFACE_OUT -o $IFACE_IN -m state --state ESTABLISHED,RELA
 --title 'FAIL' \
 --msgbox 'ERROR: Internet not Shared. Please verify all proccess again!!!' \
 0 0 
+
+iptables -L -n |egrep "FORWARD"
+
+else
+{ dialog \
+--title 'WARNING' \
+--msgbox 'ERROR: value must be '1' or '0'!!! ' \
+0 0;exit; }
 fi
-clear
-reset 
+
 
 
